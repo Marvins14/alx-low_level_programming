62d1f2fcb48161e6150e134bd1562ed074cd5f8b
Your Name <you@example.com>
2022-06-03 11:01:02 -0700

    correction

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,10 +15,10 @@ int main(void)
 	if (n < 0)
 	{
 		printf("%d is negative\n", n);
-	}else if (n == 0)
+	} else if (n == 0)
 	{
 		printf("%d is zero\n", n);
-	}else
+	} else
 	{
 		printf("%d is positive\n", n);
 	}
