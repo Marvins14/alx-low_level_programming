edd45c788a100a4afa2df102eae396e445455dbc
Your Name <you@example.com>
2022-06-02 20:59:39 -0700

    advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,8 @@
 # include<unistd.h>
 
 
-/**main - Entry point
+/**
+ * main - Entry point
  *
  * Return: Always 1(Success)
  */
@@ -10,7 +11,9 @@
 int main(void)
 {
 
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,
+	
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	
 	return (1);
 }
