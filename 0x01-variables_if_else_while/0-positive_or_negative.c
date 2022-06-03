9d07e18ae0a93c4e914ecf0bd83bf4af1c634f4b
Your Name <you@example.com>
2022-06-03 09:59:33 -0700

    correction

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*main-Entry point
- * Return:Always 0(success)
- * */
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int n;
