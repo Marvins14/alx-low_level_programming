2c572f648bc0132c5ed6867d5782c5bdeb41f32a
Your Name <you@example.com>
2022-06-03 09:46:07 -0700

    correction

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,15 +4,13 @@
 /*main-Entry point
  * Return:Always 0(success)
  * */
-
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	
 	if (n < 0)
 	{
 		printf("%d is negative\n", n);
