31e0b34b0c429143ee06cab6604ae5c3cc4946f7
Your Name <you@example.com>
2022-06-03 10:46:28 -0700

    correction

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,13 +2,14 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Entry point:
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Success):
  */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;	
 	if (n < 0)
