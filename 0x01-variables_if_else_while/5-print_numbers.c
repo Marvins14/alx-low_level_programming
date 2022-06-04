9c69f1343ea3f344e051b9cdf2b0eb8a8fa49fdd
Your Name <you@example.com>
2022-06-03 17:44:06 -0700

    print

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ *main - entry point
  *
- * main - entry point
- *
- * return: always 0(success)
+ *return: always 0(success)
  **/
 int main(void)
 {
