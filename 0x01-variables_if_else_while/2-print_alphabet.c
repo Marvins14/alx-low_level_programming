e8a3dec957be9cc02fc23ff23e74025112432c71
NKORO MACILINUS <104755074+Marvins14@users.noreply.github.com>
2022-06-06 03:13:19 +0100

    updated

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	char a;
+char a;
 
-	for (a = 'a'; a <= 'z' ; a++)
-	putchar(a);
-	putchar('\n');
-	return (0);
+for (a = 'a'; a <= 'z' ; a++)
+putchar(a);
+putchar('\n');
+return (0);
 }
