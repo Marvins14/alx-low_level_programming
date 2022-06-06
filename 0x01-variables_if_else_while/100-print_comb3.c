11068180bb5e9c24c450d82a8829a5308a980f10
NKORO MACILINUS <104755074+Marvins14@users.noreply.github.com>
2022-06-06 03:31:47 +0100

    updated

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,23 +7,23 @@
  */
 int main(void)
 {
-	int i;
-	int j;
+int i;	
+int j;
 
-	for (i = 10; i <= 19; i++)
-	{
-		for (j = 10; j <= 19; j++)
-		{
-			putchar((i % 10) + '0');
-			putchar((j % 10) + '0');
-			if (i != 19 ||  j != 19)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+for (i = 10; i <= 19; i++)
+{
+for (j = 10; j <= 19; j++)
+{
+putchar((i % 10) + '0');
+putchar((j % 10) + '0');
+if (i != 19 ||  j != 19)
+{
+putchar(',');
+putchar(' ');
+}
 
-		}
-	}
-	putchar('\n');
-	return (0);
+}
+}
+putchar('\n');
+return (0);
 }
