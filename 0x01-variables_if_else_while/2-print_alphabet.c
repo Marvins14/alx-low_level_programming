7cec738dbf3c673e7880dc66ec9194d28e4cc629
Your Name <you@example.com>
2022-06-05 18:32:49 -0700

    getin along

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * return: Always 0(success)
+ */
+int main(void)
+{
+	char a;
+	for (a = 'a'; a <= 'z' ; a++)
+	putchar(a);
+	putchar('\n');
+	return (0);
+}
