de5c448a9c21f9ec50abeec9c3150539697d8305
Your Name <you@example.com>
2022-06-06 18:01:54 -0700

    corrected version

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,45 +9,23 @@ int main(void)
 {
 int i;	
 int j;
+
 for (i = 0; i <= 9; i++)
-
-{
-
-for (j = 0; j <= 9; j++)
-
+for (j = 1; j <= 9; j++)
 {
 {
-
 if (j > i)
-
 {
-
 putchar(i + '0');
-
 putchar(j + '0');
-
-if (i != 7 || j != 8)
-
+if (i != 0 || j != 0)
 {
-
 putchar(',');
-
 putchar(' ');
-
 }
-
 }
-
 }
-
 }
-
-}
-
-
-
 putchar('\n');
-
 return (0);
-
 }
