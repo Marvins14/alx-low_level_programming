4a09aa81b4247b698e55bcb3689b01c7cf2c78f1
Your Name <you@example.com>
2022-06-07 15:10:46 -0700

    messages

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - entry point
+ * main - Print _putchar
  *
+ * followed by a new line
  * Return: Always return 0
  */
 int main(void)
 
 {
-	_putchar("_putchar\n");
-	return (0);
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return (0);
 }
