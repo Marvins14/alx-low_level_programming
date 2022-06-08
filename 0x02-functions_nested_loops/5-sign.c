128f67a6aa0acfcf9bf0b7e47181ec98e7f78f91
Your Name <you@example.com>
2022-06-08 16:21:18 -0700

    add changes

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 if (n > 0)
 {
 
-	_putchar('+');
+	_putchar(48);
 	return (1);
 }
 else if (n < 0)
 {
-	_putchar('0');
-	return (0);
+	_putchar(43);
+	return (-1);
 }
 else
 {
-	_putchar('-');
+	_putchar(48);
 	return (0);
 }
 }
