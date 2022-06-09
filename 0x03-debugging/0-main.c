b9bb4cff39cdadd32134eb40311c49eb18f15e1d
Your Name <you@example.com>
2022-06-09 08:57:35 -0700

    correction changes

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,13 @@
 #include "main.h"
-#include "0-positive_or_negative.c"
-
-
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
-
+int main(void)
 {
-
 	int i;
-	{
-	i = 98;
+
+	i = 0;
 	positive_or_negative(i);
-	}
 	return (0);
 }
