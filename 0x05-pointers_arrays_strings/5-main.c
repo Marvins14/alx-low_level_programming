20e59c7f8bc9f04f6e38fd3d52a468fedb2ec924
NKORO MACILINUS <104755074+Marvins14@users.noreply.github.com>
2022-06-15 03:19:26 +0100

    save changes

--- 0x05-pointers_arrays_strings/5-main.c
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -1,24 +1,16 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * main - check the code
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
-
 {
+    char s[10] = "My School";
 
-	    char s[10] = "My School";
-
-
-
-	        printf("%s\n", s);
-
-		    rev_string(s);
-
-		        printf("%s\n", s);
-
-			    return (0);
-
-}
+    printf("%s\n", s);
+    rev_string(s);
+    printf("%s\n", s);
+    return (0);
