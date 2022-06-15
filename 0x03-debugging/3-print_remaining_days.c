e03878e99f5b82ddeead749069ee756cc8c3208b
Your Name <you@example.com>
2022-06-14 19:05:27 -0700

    add changes

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -8,7 +8,7 @@
  * @year: year
  * Return: void
  */
-int print_remaining_days(int month, int day, int year)
+void print_remaining_days(int month, int day, int year)
 {
 
 	    if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
