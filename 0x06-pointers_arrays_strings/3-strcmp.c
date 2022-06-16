efbf9297d449081b33e219c43f867323bf077e3f
Your Name <you@example.com>
2022-06-16 00:49:12 -0700

    add file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,4 +2,21 @@
 /**
  * _strcmp - Compares pointers to  two strings
  * @s1: A pointer to the first string to be compared.
- * @s2: 
+ * @s2: A pointer to the second string to be compared.
+ *
+ * Return:0, if the s1 and s2 are equal;
+ * a negative value if s1 is less than s2;
+ * a positive value if s1 is greater than s2.
+ *
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while
+		((*s1 && *s2) && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+
+}
