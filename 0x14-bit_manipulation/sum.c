6b1f045c6734cb8ab71b20d2c20627cb6fb9f6d3
Marvins14 <nkoromacilinus@gmail.com>
2022-07-22 21:07:56 -0700

    add

---
+++ 0x14-bit_manipulation/sum.c
@@ -0,0 +1,15 @@
+nclude <stdio.h>
+
+int main(void){
+	    int sum(int x, int y)
+		        int n
+			    n = (2,3);
+	        printf("%d\n", n);
+
+		    return(0)
+}
+int sum(int x, int y){
+	    int result;
+	        result = x + y;
+		    return(result)
+}
