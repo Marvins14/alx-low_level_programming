c985d5bdd04e8f284eb516ec03e2b24536997fe3
NKORO MACILINUS <104755074+Marvins14@users.noreply.github.com>
2022-07-30 00:40:10 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,30 +1,45 @@
-#include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 /**
- * read_textfile - reads a text file and prints it to the standard output
- * @filename: the file name to be read
- * @letters: number of letters to read and print
- * Return: the number of letters read and print
+ * read_textfile - Function that reads a textfile and
+ * prints it to the POSIX standard output
+ * @filename: pointer to the filename
+ * @letters: Number of letters to be printed
+ * Return: Actual number of letters it could read and print
+ * or 0 if filename is NULL, file cant be opened or read
+ * or if fails or does not write the expected amount of bytes
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int filetxt;
-	ssize_t nrd, nwr;
+	int fd;
+	ssize_t writecount, readcount;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-	buffer = malloc(sizeof(char) * (letters));
-	if (buffer == NULL)
+
+	fd = open(filename, O_RDWR);
+
+	if (fd == -1)
 		return (0);
-
-	nrd = read(filetxt, buffer, letters);
-	nwr = write(STDOUT_FILENO, buffer, nrd);
-
-	close(filetxt);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
+	readcount = read(fd, buffer, letters);
+	if (readcount == -1)
+		return (0);
+	writecount = write(STDOUT_FILENO, buffer, readcount);
+	if (writecount == -1 || writecount != readcount)
+		return (0);
 	free(buffer);
-	return (nwr);
-
+	close(fd);
+	return (writecount);
 }
-
